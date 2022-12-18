345b82bbf62fcf18ce409ff8cff6ec880aa53ea3
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-12-18 15:23:52 +0000

    [finish]request_repair.12/19

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :engineers,skip: [:passwords], controllers: {
+    registrations: "engineer/registrations",
+    sessions: 'engineer/sessions'
+  }
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+}
 end
