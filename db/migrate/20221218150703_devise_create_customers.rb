0946dac7efeb3a57ee76e942a50c888df21606fc
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-12-20 18:07:49 +0000

    [finish]request_repair.12/20

--- db/migrate/20221218150703_devise_create_customers.rb
+++ db/migrate/20221218150703_devise_create_customers.rb
@@ -10,6 +10,17 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
+      
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :telephone_number, null: false
+      t.boolean :is_deleted, null: false, default: false
+      
+      
 
       ## Rememberable
       t.datetime :remember_created_at
--- db/migrate/20221218150740_devise_create_engineers.rb
+++ db/migrate/20221218150740_devise_create_engineers.rb
@@ -6,6 +6,12 @@ class DeviseCreateEngineers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :telephone_number, null: false
+      t.boolean :engineers_deleted, null: false, default:false
 
       ## Recoverable
       t.string   :reset_password_token
