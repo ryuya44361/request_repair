4374f0aaeb6fa86c5c51ebd770a85d436a75f706
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-12-21 15:19:09 +0000

    [finish]request_repair.12/21

--- app/controllers/engineer/registrations_controller.rb
+++ app/controllers/engineer/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Engineer::RegistrationsController < Devise::RegistrationsController
- before_action :configure_permitted_parameters, if: :devise_controller?
+ before_action :engineer_permitted_parameters, if: :devise_controller?
  
   def after_sign_in_path_for(resource)
     engineer_path
@@ -9,7 +9,7 @@ class Engineer::RegistrationsController < Devise::RegistrationsController
  
   protected
 
-    def configure_permitted_parameters
+    def engineer_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:telephone_number])
     end
 end
