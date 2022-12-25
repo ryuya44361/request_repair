a9f5d841a4976eec644c1c5492989819dbebfdaa
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2022-12-25 03:42:41 +0000

    [finish]request_repair.12/25

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,7 +4,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
  
   def after_sign_in_path_for(resource)
-    customers_path
+    customer_session_path
   end
  
   protected
