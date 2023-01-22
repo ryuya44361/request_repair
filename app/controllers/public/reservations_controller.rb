eb1047eb89658cc5354bb77c23fbaafead58e1e3
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2023-01-22 15:41:39 +0000

    [finish]request_repair.01/22

--- app/controllers/public/reservations_controller.rb
+++ app/controllers/public/reservations_controller.rb
@@ -1,11 +1,11 @@
 class Public::ReservationsController < ApplicationController
-  
+
   def day
     @default_limits = DefaultLimit.all
     @restrictions = Restriction.all
-    
+
   end
-  
+
   def time
     @day_params = params[:format]
     @default_limits = DefaultLimit.all
@@ -17,38 +17,41 @@ class Public::ReservationsController < ApplicationController
         @restriction = Restriction.where(reservation_day: @day_params)
       end
   end
-  
+
   def new
     @reservation = Reservation.new
+    @day_params = params[:reservation_day]
+    @start_time_params = params[:start_time]
+    @finish_time_params = params[:finish_time]
     @genres = Genre.all
   end
-  
+
   def confirm
     @reservation = Reservation.new(reservations_parameters)
   end
-  
+
   def complete
   end
-  
+
   def create
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   def destroy
   end
-  
+
   def cancel
   end
-  
+
   protected
 
     def reservations_parameters
-      params.require(:reservation).permit(:customer_id,:engineer_id,:genre_id,:completion_id,:image,:reservation_day,:start_time,:finish_time,:model_number,:serial_number,:introduction)
+      params.permit(:customer_id,:engineer_id,:genre_id,:completion_id,:image,:reservation_day,:start_time,:finish_time,:model_number,:serial_number,:introduction)
     end
-  
+
 end
